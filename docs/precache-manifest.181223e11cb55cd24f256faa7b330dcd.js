0f50d9e825d64bcd199e095d72018213f094b02a
Nelo Puchades <correo@nelo.dev>
2020-09-27 18:31:24 +0200

    Changed public to docs folder

---
+++ docs/precache-manifest.181223e11cb55cd24f256faa7b330dcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0fedf4b2a592541f10fab3c416aad04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4386e2ae7a17e89bdbf6",
+    "url": "/static/css/main.9e7d71b5.chunk.css"
+  },
+  {
+    "revision": "76da65e5c0c7af143bb0",
+    "url": "/static/js/2.e8c6f352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8c6f352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4386e2ae7a17e89bdbf6",
+    "url": "/static/js/main.5b182ed9.chunk.js"
+  },
+  {
+    "revision": "8aa548225c1bf62340ca",
+    "url": "/static/js/runtime-main.7c2dabf1.js"
+  }
+]);
